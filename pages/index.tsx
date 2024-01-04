fe3e251af58a5dd58f995e25bd4938fa8c849f2e
Sikandar Abbas <sikiawan1@gmail.com>
2024-01-04 22:07:16 +0500

    theme and language change work

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { DatePicker } from '@/components/ui/date-picker';
 import { Inter } from 'next/font/google';
 import React from 'react';
-import { useTranslation, } from 'react-i18next'
+import { useTranslation, } from 'next-i18next'
 import {
   Select,
   SelectContent,
@@ -10,30 +10,95 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { useTheme } from 'next-themes';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { useRouter } from 'next/router';
+import { Value } from '@radix-ui/react-select';
 const inter = Inter({ subsets: ['latin'] });
 
 const languages = [
   { code: 'en', translateKey: 'english' },
-  { code: 'pt', translateKey: 'portuguese' },
+  { code: 'ar', translateKey: 'arabic' },
   { code: 'es', translateKey: 'spanish' },
 ]
+const themes = [
+  { code: 'default', translateKey: 'default' },
+  { code: 'dark', translateKey: 'dark' },
+]
 export default function Home() {
+  const { locale } = useRouter();
+  const route = useRouter();
+
+  const { resolvedTheme, theme, setTheme } = useTheme();
   const [date, setDate] = React.useState<Date>();
   const { t, i18n, } = useTranslation('common')
   const changeLanguage = (language: string) => {
-
     i18n.changeLanguage(language)
   }
+  const changeTheme = (them: string) => {
+    document.querySelector("html")?.setAttribute("data-theme", them);
+  };
 
   return (
     <div className='flex h-screen w-full items-center justify-center'>
+      <div className=' grid place-items-center'>
+        <div>
+          <Button className='py-2 px-8 m-2' onClick={() => changeTheme("default")}>default</Button>
+          <Button className='py-2 px-8 m-2' onClick={() => changeTheme("dark")}>dark</Button>
+        </div>
+        <div>
+          <Button className='py-2 px-8 m-2' onClick={() => changeLanguage("en")}>English</Button>
+          <Button className='py-2 px-8 m-2' onClick={() => changeLanguage("ar")}>Urdu</Button>
+        </div>
+        <div className='flex flex-row gap-3'>
+          <Select
+            value={locale}
+            onValueChange={(newValue) => {
+              route.push('', undefined, {
+                locale: newValue,
+              }).then(() => {
+                //route.reload();
+              });
+            }}
+          >
+            <SelectTrigger>
+              {languages.find((op) => op.code === locale)?.translateKey}
+            </SelectTrigger>
+            <SelectContent>
+              {languages.map((op) => (
+                <SelectItem value={op.code} key={op.code}>
+                  {op.translateKey}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
+          <Select
+          value={theme}
+            onValueChange={(newValue) => {
+              changeTheme(newValue);
+            }}
+          >
+            <SelectTrigger>
+              {themes.find((op) => op.code === document.querySelector("html")?.getAttribute("data-theme"))?.translateKey}
+            </SelectTrigger>
+            <SelectContent>
+              {themes.map((op) => (
+                <SelectItem value={op.code} key={op.code}>
+                  {op.translateKey}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
       <div className='grid gap-8'>
         <DatePicker date={date} setDate={setDate} />
         <Button variant='destructive'>hello</Button>
       </div>
       <Select>
         <SelectTrigger className="w-[180px]">
-      
+
         </SelectTrigger>
         <SelectContent>
           {languages.map((language, index) => (
@@ -43,6 +108,18 @@ export default function Home() {
 
         </SelectContent>
       </Select>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-[180px]">
+
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {themes.map((theme, index) => (
+            <DropdownMenuItem onChange={() => changeTheme(theme.code)} onSelect={() => changeTheme(theme.code)} key={index} defaultChecked={theme.code == 'default'}  >{theme.translateKey}</DropdownMenuItem>
+          ))}
+
+
+        </DropdownMenuContent>
+      </DropdownMenu>
       <div>
         {/* {languages.map((language) => (
           <button
